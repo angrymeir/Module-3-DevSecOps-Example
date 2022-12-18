7c8ba5e718beb25f15be6b96c6f4365f5804f927
aifos111 <113522423+aifos111@users.noreply.github.com>
2022-12-18 12:58:40 +0100

    Introduce vulns again

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ repo = os.getenv('REPO')
 workflow_runs_url = "https://api.github.com/repos/{}/actions/runs"
 workflows = ['Container Scanning with Trivy', 'SAST with Bandit', 'Secrets scanning with GitLeaks']
 
+def never_called(bla):
+    aws_access_token = "AKIALALEMEL33243OLIB"
+    os.subprocess.Popen('echo ${}'.format(aws_access_token), shell=True)
 
 def serve_image(state):
     return send_file("bla.jpeg", mimetype="image/png")
@@ -26,7 +29,7 @@ def hello_world():
 
 
 def main():
-    app.run(debug=False)
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
