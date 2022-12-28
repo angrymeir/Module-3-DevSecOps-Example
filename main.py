c2c349dbb717e51922e82b2b1d95caf5feda84e7
saldoms <simme_an@hotmail.com>
2022-12-28 12:19:05 +0100

    remove secret

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ workflows = ['Container Scanning with Trivy', 'SAST with Bandit', 'Secrets scann
 
 
 def never_called(bla):
-    aws_access_token = "AKIALALEMEL33243OLIB"
+    aws_access_token = ""
     os.subprocess.Popen('echo ${}'.format(aws_access_token), shell=True)
 
 
