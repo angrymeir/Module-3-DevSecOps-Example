62d0fd7ce7e3e05a41da3302c735af01c3761aa1
BhavyaChaudhary26 <84770705+BhavyaChaudhary26@users.noreply.github.com>
2023-01-12 14:04:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ workflows = ['Container Scanning with Trivy', 'SAST with Bandit', 'Secrets scann
 
 
 def never_called(bla):
-    aws_access_token = "AKIALALEMEL33243OLIB"
-    os.subprocess.Popen('echo ${}'.format(aws_access_token), shell=True)
+    # aws_access_token = "AKIALALEMEL33243OLIB"
+    os.subprocess.Popen('echo ${}'.format(aws_access_token), shell=False)
 
 
 def serve_image(state):
@@ -31,7 +31,7 @@ def hello_world():
 
 
 def main():
-    app.run(debug=True)
+    app.run(debug=False)
 
 
 if __name__ == '__main__':
