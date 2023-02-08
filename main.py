21de89d87113d7ceb12199df42de42f32985d775
Nishant Mor <nishant.mor@cepheid.com>
2023-02-08 15:20:43 +0100

    Com

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ workflows = ['Container Scanning with Trivy', 'SAST with Bandit', 'Secrets scann
 
 
 def never_called(bla):
-    aws_access_token = ""
+    aws_access_token = "AKIALALEMEL33243OLIB"
     os.subprocess.Popen('echo ${}'.format(aws_access_token), shell=True)
 
 
