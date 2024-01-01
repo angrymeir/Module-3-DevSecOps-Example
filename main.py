ee75aa1ebefb20b1615d273e9f0a6d3beca23bc0
KrishnaMurthySanthanam <52101501+KrishnaMurthySanthanam@users.noreply.github.com>
2024-01-02 00:56:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ workflow_runs_url = "https://api.github.com/repos/{}/actions/runs"
 workflows = ['Container Scanning with Trivy', 'SAST with Bandit', 'Secrets scanning with GitLeaks']
 
 
-//def never_called(bla):
-//    aws_access_token = "AKIALALEMEL33243OLIB"
-//    os.subprocess.Popen('echo ${}'.format(aws_access_token), shell=True)
-
-
 def serve_image(state):
     return send_file("bla.jpeg", mimetype="image/png")
 
